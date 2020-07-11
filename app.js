6a48644d8f520b0938e93f0f480d0cf381c3af88
lucas.oliveira <lucasoliveira4@msn.com>
2020-07-11 03:58:50 -0300

    turning back

--- app.js
+++ app.js
@@ -25,10 +25,7 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 app.use(
   cors({
     origin: 'https://desafio-modulo-4-api.herokuapp.com/grade',
